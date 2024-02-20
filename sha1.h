97ec6bc1a9b99b08c3b54c39a8f82f82594b4379
smile31768 <60568280+smile31768@users.noreply.github.com>
2024-02-21 00:50:21 +0800

    Update sha1.h

--- sha1.h
+++ sha1.h
@@ -1,3 +1,5 @@
+#ifndef SHA1_H_
+#define SHA1_H_
 #include <inttypes.h>
 
 #define HASH_LENGTH 20
@@ -23,3 +25,5 @@ uint8_t* result(void);
 uint8_t* resultHmac(void);
 void write(uint8_t);
 void writeArray(uint8_t *buffer, uint8_t size);
+
+#endif    //SHA1_H_
